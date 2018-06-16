648c5db1aeb02cda1659ec56956e8922c9d5352f
hyunsu <coding8282@gmail.com>
2018-06-17 01:52:48 +0900

    샘플 추가

--- src/main/java/org/okky/notification/domain/model/Article.java
+++ src/main/java/org/okky/notification/domain/model/Article.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.okky.share.JsonUtil;
 import org.okky.share.domain.ValueObject;
 
 import static lombok.AccessLevel.PRIVATE;
@@ -16,4 +17,16 @@ public class Article implements ValueObject {
     String id;
     String writerId;
     String writerName;
+
+    public static void main(String[] args) {
+        System.out.println(JsonUtil.toPrettyJson(sample()));
+    }
+
+    public static Article sample() {
+        String id = "a-1";
+        String writerId = "m-3";
+        String writerName = "coding8282";
+        Article article = new Article(id, writerId, writerName);
+        return article;
+    }
 }
