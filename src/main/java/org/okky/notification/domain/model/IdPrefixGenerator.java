e1695c4e23ce822ec5f8fa496d414e97b4ffaa9f
hyunsu <coding8282@gmail.com>
2018-06-29 19:39:35 +0900

    id 생성기 수정
    - prefix 규칙 수정.
    - ReplyPinnedNoti 추가.

--- src/main/java/org/okky/notification/domain/model/IdPrefixGenerator.java
+++ src/main/java/org/okky/notification/domain/model/IdPrefixGenerator.java
@@ -1,7 +1,17 @@
 package org.okky.notification.domain.model;
 
+import java.util.UUID;
+
 interface IdPrefixGenerator {
-    static String replyWroteNotiId() {
-        return "nr";
+    static String nextReplyWroteNotiId() {
+        return "nr" + uuid();
+    }
+
+    static String nextReplyPinnedNotiId() {
+        return "np-" + uuid();
+    }
+
+    static String uuid() {
+        return UUID.randomUUID().toString().replaceAll("-", "").substring(0, 15);
     }
 }
