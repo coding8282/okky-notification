286ba5a78ad05fcea75bb35bef960072b1357c1e
hyunsu <coding8282@gmail.com>
2018-06-30 14:25:54 +0900

    Notification 리파지터리를 통합하면서 사용하지 않는 소스 삭제

--- src/main/java/org/okky/notification/domain/repository/ReplyWroteNotiRepository.java
+++
@@ -1,6 +0,0 @@
-package org.okky.notification.domain.repository;
-
-import org.okky.notification.domain.model.ReplyWroteNoti;
-
-public interface ReplyWroteNotiRepository extends BaseRepository<ReplyWroteNoti> {
-}
