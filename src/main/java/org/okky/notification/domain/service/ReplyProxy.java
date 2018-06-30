725b84e3fb35e5f10caaa4983a4104428035cca9
hyunsu <coding8282@gmail.com>
2018-07-01 00:25:51 +0900

    ReplyProxy 리팩토링
    - 클라이언트 코드 편의를 위하여 List -> Set을 반환하도록 수정.

--- src/main/java/org/okky/notification/domain/service/ReplyProxy.java
+++ src/main/java/org/okky/notification/domain/service/ReplyProxy.java
@@ -5,7 +5,7 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
+import java.util.Set;
 
 import static java.lang.String.format;
 import static lombok.AccessLevel.PRIVATE;
@@ -16,8 +16,12 @@ import static lombok.AccessLevel.PRIVATE;
 public class ReplyProxy {
     RestTemplate template;
 
-    public List<String> fetchReplierIds(String articleId, int page) {
+    public Set<String> fetchReplierIds(String articleId) {
+        return fetchReplierIds(articleId, 0);
+    }
+
+    public Set<String> fetchReplierIds(String articleId, int page) {
         String url = format("/articles/%s/repliers?page=%d", articleId, page);
-        return template.getForEntity(url, List.class).getBody();
+        return template.getForEntity(url, Set.class).getBody();
     }
 }
