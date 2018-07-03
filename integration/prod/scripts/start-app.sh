6079a7bacc11700ab93a41a7b05912948a022472
hyunsu <coding8282@gmail.com>
2018-07-04 06:15:17 +0900

    운영환경설정 이관

--- integration/prod/scripts/start-app.sh
+++ integration/prod/scripts/start-app.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-nohup java -jar \
-/home/ec2-user/okky-notification-1.0.0.jar \
-> /dev/null 2> /dev/null < /dev/null &
+sudo chmod +x /home/ec2-user/okky-notification-1.0.0.jar
+sudo ln -sf /home/ec2-user/okky-notification-1.0.0.jar /etc/init.d/okky-notification
+sudo service okky-notification start
+sleep 10s
