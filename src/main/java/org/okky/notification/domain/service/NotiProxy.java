e91cfd10cedcf68fd6be73a8e5d87b837fb4c04f
hyunsu <coding8282@gmail.com>
2018-07-05 03:20:18 +0900

    NotiProxy에 fetchScrapper를 추가

--- src/main/java/org/okky/notification/domain/service/NotiProxy.java
+++ src/main/java/org/okky/notification/domain/service/NotiProxy.java
@@ -2,10 +2,7 @@ package org.okky.notification.domain.service;
 
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.okky.notification.domain.model.Article;
-import org.okky.notification.domain.model.Emoter;
-import org.okky.notification.domain.model.Member;
-import org.okky.notification.domain.model.Reply;
+import org.okky.notification.domain.model.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -28,6 +25,11 @@ public class NotiProxy {
     public Emoter fetchEmoter(String emoterId) {
         String url = "/members/" + emoterId;
         return template.getForEntity(url, Emoter.class).getBody();
+    }
+
+    public Scrapper fetchScrapper(String scrapperId) {
+        String url = "/members/" + scrapperId;
+        return template.getForEntity(url, Scrapper.class).getBody();
     }
 
     public Article fetchArticle(String articleId) {
